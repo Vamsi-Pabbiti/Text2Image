df65acc671c58a986419b7ccfec4484b237670ff
my token <vamsipabbiti2004@gmail.com>
2026-05-12 14:42:46 +0530

    fix port for render

--- app.py
+++ app.py
@@ -261,4 +261,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
